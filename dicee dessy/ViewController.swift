c80a025bd99562e9fac804b6b6d97e8fa38b74f6
mlutfiibra <mlutfiibra@outlook.com>
2019-10-08 07:39:52 +0700

    Initial Commit

---
+++ dicee dessy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dicee dessy
+//
+//  Created by lutfi on 08/10/19.
+//  Copyright © 2019 mlutfiibra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

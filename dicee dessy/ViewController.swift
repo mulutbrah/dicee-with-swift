2198c00e6fd524bd8c716177a3e186f2e8737c03
mlutfiibra <mlutfiibra@outlook.com>
2019-10-09 21:33:33 +0700

    Random image

--- dicee dessy/ViewController.swift
+++ dicee dessy/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
+    let diceCollection = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -24,6 +26,9 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named : diceCollection[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceCollection[randomDiceIndex2])
     }
     
 }
